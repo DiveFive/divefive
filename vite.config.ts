e8d08476d734060750a983b67cdacd3c89e8ba2e
Andrea Blass <andreablass11@gmail.com>
2025-09-24 19:32:31 -0600

    Update vite.config.ts
    Deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.VERCEL ? '/' : '/DiveFive-webapp/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
